d498745dead226ac501ef24e0f9f08364e720286
LuanEleuterio <luaneleuterio3@hotmail.com>
2021-01-18 13:42:40 -0300

    Adicionado slide de elementos na pagina Portfolio e feita proteção para não quebrar o site caso o js esteja desabilitado

---
+++ js/script.js
@@ -0,0 +1,14 @@
+if(window.SimpleSlide){
+  new SimpleSlide({
+    slide: "quote",
+    auto: true,
+    time: 5000
+  })
+
+  new SimpleSlide({
+    slide: "portfolio-slide",
+    auto: true,
+    time: 5000,
+    nav:true
+  })
+}
